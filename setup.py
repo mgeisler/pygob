46ab9438b4099e66f7ec582d6c035cc6d75f4e10
Martin Geisler <martin@geisler.net>
2017-03-09 07:21:00 +0100

    Initial skeleton

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pygob',
+    version='0.1',
+    description='Python implementation of the Go gob format',
+    url='http://github.com/mgeisler/pygob',
+    author='Martin Geisler',
+    author_email='martin@geisler.net',
+    license='MIT',
+    packages=['pygob'],
+    zip_safe=False)
