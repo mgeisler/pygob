20878df423f8c65358bc8683acbebaf5c28fae8c
Martin Geisler <martin@geisler.net>
2017-03-11 11:07:28 +0100

    tests: add simple int and uint tests using pytest

---
+++ tests/test_load.py
@@ -0,0 +1,32 @@
+import pytest
+
+import pygob
+
+
+@pytest.mark.parametrize(('data', 'expected'), [
+    ([3, 6, 0, 1], 1),
+    ([3, 6, 0, 2], 2),
+    ([3, 6, 0, 3], 3),
+    ([4, 6, 0, 255, 255], 255),
+    ([5, 6, 0, 254, 1, 0], 256),
+    ([5, 6, 0, 254, 1, 1], 257),
+])
+def test_uint(data, expected):
+    assert pygob.load(bytes(data)) == expected
+
+
+@pytest.mark.parametrize(('data', 'expected'), [
+    ([3, 4, 0, 5], -3),
+    ([3, 4, 0, 3], -2),
+    ([3, 4, 0, 1], -1),
+    ([3, 4, 0, 0], 0),
+    ([3, 4, 0, 2], 1),
+    ([3, 4, 0, 4], 2),
+    ([3, 4, 0, 6], 3),
+    ([5, 4, 0, 254, 1, 255], -256),
+    ([5, 4, 0, 254, 1, 253], -255),
+    ([5, 4, 0, 254, 1, 254], 255),
+    ([5, 4, 0, 254, 2, 0], 256),
+])
+def test_int(data, expected):
+    assert pygob.load(bytes(data)) == expected
