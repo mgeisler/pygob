4b52686d519910b4eddf7f4452b5065224ac6279
Martin Geisler <martin@geisler.net>
2017-03-13 21:48:25 +0100

    Loader: move decode_X methods to separate classes
    We now have a Python class per Go type. These classes will grow a
    little next and some classes (such as Go array and structs) will be
    parameterized so as to model different concrete Go types.

--- pygob/loader.py
+++ pygob/loader.py
@@ -6,20 +6,20 @@ from .types import TypeID
 class Loader:
     def __init__(self):
         self._decoders = {
-            TypeID.INT: self.decode_int,
-            TypeID.UINT: self.decode_uint,
-            TypeID.BOOL: self.decode_bool,
-            TypeID.FLOAT: self.decode_float,
-            TypeID.BYTE_SLICE: self.decode_byte_slice,
-            TypeID.STRING: self.decode_string,
-            TypeID.COMPLEX: self.decode_complex,
+            TypeID.INT: GoInt.decode,
+            TypeID.UINT: GoUint.decode,
+            TypeID.BOOL: GoBool.decode,
+            TypeID.FLOAT: GoFloat.decode,
+            TypeID.BYTE_SLICE: GoByteSlice.decode,
+            TypeID.STRING: GoString.decode,
+            TypeID.COMPLEX: GoComplex.decode,
         }
 
     def load(self, buf):
-        length, buf = self.decode_uint(buf)
+        length, buf = GoUint.decode(buf)
         assert len(buf) == length
 
-        typeid, buf = self.decode_int(buf)
+        typeid, buf = GoInt.decode(buf)
         if typeid < 0:
             raise NotImplementedError("cannot decode non-standard type ID %d" %
                                       -typeid)
@@ -28,11 +28,32 @@ class Loader:
         value, buf = self.decode_value(typeid, buf[1:])
         return value
 
-    def decode_bool(self, buf):
-        n, buf = self.decode_uint(buf)
+    def decode_value(self, typeid, buf):
+        decoder = self._decoders.get(typeid)
+        if decoder is None:
+            raise NotImplementedError("cannot decode %s" % typeid)
+        return decoder(buf)
+
+
+class GoType:
+    """Represents a Go type.
+
+    Go types know how to decode a gob stream to their corresponding
+    Python type.
+    """
+    pass
+
+
+class GoBool(GoType):
+    @staticmethod
+    def decode(buf):
+        n, buf = GoUint.decode(buf)
         return n == 1, buf
 
-    def decode_uint(self, buf):
+
+class GoUint(GoType):
+    @staticmethod
+    def decode(buf):
         (length, ) = struct.unpack('b', buf[:1])
         if length >= 0:  # small uint in a single byte
             return length, buf[1:]
@@ -45,36 +66,45 @@ class Loader:
         n += buf[length]
         return n, buf[length + 1:]
 
-    def decode_int(self, buf):
-        uint, buf = self.decode_uint(buf)
+
+class GoInt(GoType):
+    @staticmethod
+    def decode(buf):
+        uint, buf = GoUint.decode(buf)
         if uint & 1:
             uint = ~uint
         return uint >> 1, buf
 
-    def decode_float(self, buf):
-        n, buf = self.decode_uint(buf)
+
+class GoFloat(GoType):
+    @staticmethod
+    def decode(buf):
+        n, buf = GoUint.decode(buf)
         rev = bytes(reversed(struct.pack('L', n)))
         (f, ) = struct.unpack('d', rev)
         return f, buf
 
-    def decode_byte_slice(self, buf):
-        count, buf = self.decode_uint(buf)
+
+class GoByteSlice(GoType):
+    @staticmethod
+    def decode(buf):
+        count, buf = GoUint.decode(buf)
         return bytearray(buf[:count]), buf[count:]
 
-    def decode_string(self, buf):
-        count, buf = self.decode_uint(buf)
+
+class GoString(GoType):
+    @staticmethod
+    def decode(buf):
+        count, buf = GoUint.decode(buf)
         # TODO: Go strings do not guarantee any particular encoding.
         # Add support for trying to decode the bytes using, say,
         # UTF-8, so we can return a real Python string.
         return buf[:count], buf[count:]
 
-    def decode_complex(self, buf):
-        re, buf = self.decode_float(buf)
-        im, buf = self.decode_float(buf)
-        return complex(re, im), buf
 
-    def decode_value(self, typeid, buf):
-        decoder = self._decoders.get(typeid)
-        if decoder is None:
-            raise NotImplementedError("cannot decode %s" % typeid)
-        return decoder(buf)
+class GoComplex(GoType):
+    @staticmethod
+    def decode(buf):
+        re, buf = GoFloat.decode(buf)
+        im, buf = GoFloat.decode(buf)
+        return complex(re, im), buf
