5929c1f9f7566ba13b7a5001ede678634b8444aa
Martin Geisler <martin@geisler.net>
2017-03-19 22:16:43 +0100

    test_load_all: run file through yapf

--- tests/test_load_all.py
+++ tests/test_load_all.py
@@ -12,7 +12,7 @@ def load_all(data):
 
 @pytest.mark.parametrize(('data', 'expected'), [
     ([3, 4, 0, 2, 3, 4, 0, 4, 3, 4, 0, 6], [1, 2, 3]),
-    ([3, 2, 0, 1, 3, 4, 0, 34, 4, 12, 0, 1, 33], [True, 17, b'!'])
+    ([3, 2, 0, 1, 3, 4, 0, 34, 4, 12, 0, 1, 33], [True, 17, b'!']),
 ])
 def test_basic_types(data, expected):
     assert load_all(data) == expected
@@ -22,7 +22,7 @@ def test_custom_type():
     data = [
         31, 255, 131, 3, 1, 1, 5, 80, 111, 105, 110, 116, 1, 255, 132, 0, 1, 2,
         1, 1, 88, 1, 4, 0, 1, 1, 89, 1, 4, 0, 0, 0, 3, 255, 132, 0, 7, 255,
-        132, 1, 6, 1, 8, 0,
+        132, 1, 6, 1, 8, 0
     ]
     Point = collections.namedtuple('Point', ['X', 'Y'])
     assert load_all(data) == [Point(0, 0), Point(3, 4)]
